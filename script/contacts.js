66c377c303a6c045cd4655b83254ac01d3d6b1c4
Andrey Kachur <ntx033@yandex.ru>
2022-02-18 20:51:53 +0500

    email

---
+++ script/contacts.js
@@ -0,0 +1,46 @@
+// кнопка меню
+const menuButton = document.querySelector('.header__menu-button');
+// меню сайта
+const menu = document.querySelector('.header__menu');
+// кнопки меню
+const menuButtons = document.querySelectorAll('.header__menu-item');
+//Форма отправки сообщений
+const form = document.querySelector('.contacts__form');
+//поле формы имя
+const inputName = document.querySelector('.contacts__input');
+//поле формы текст
+const inputText = document.querySelector('.contact__textarea');
+
+const formData = () => {
+    const data = {};
+
+    data[input.name] = inputName.value;
+    data[input.message] = inputText.value;
+    return data;
+}
+// открываем меню
+function openMenu() {
+    menu.classList.add('header__menu_open');
+}
+
+// слушаем кнопку меню
+menuButton.addEventListener('click', openMenu);
+
+// слушаем отправку формы
+form.addEventListener('submit', function formSend(evt) {
+    evt.preventDefault();
+    fetch('sendmail.php', {
+        method: "POST",
+        body: JSON.stringify({
+            message: formData
+        }),
+    });
+    if (res.ok) {
+        return res.json();
+    } else {
+        return Promise.reject(
+            `Что-то пошло не так: Ошибка ${res.status} - ${res.statusText}`
+        );
+    }
+});
+

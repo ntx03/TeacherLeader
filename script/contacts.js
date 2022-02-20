4d3a40503b11e0fa4fe8c8e827057eca8f80fe96
Andrey Kachur <ntx033@yandex.ru>
2022-02-20 10:54:44 +0500

    edit 20/02/2022

--- script/contacts.js
+++ script/contacts.js
@@ -2,46 +2,14 @@
 const menuButton = document.querySelector('.header__menu-button');
 // меню сайта
 const menu = document.querySelector('.header__menu');
-// кнопки меню
-const menuButtons = document.querySelectorAll('.header__menu-item');
-//Форма отправки сообщений
-const form = document.querySelector('.contacts__form');
-//поле формы имя
-const inputName = document.querySelector('.contacts__input');
-//поле формы текст
-const inputText = document.querySelector('.contact__textarea');
-
-const formData = () => {
-    const data = {};
-
-    data[input.name] = inputName.value;
-    data[input.message] = inputText.value;
-    return data;
-}
 // открываем меню
 function openMenu() {
     menu.classList.add('header__menu_open');
 }
-
 // слушаем кнопку меню
 menuButton.addEventListener('click', openMenu);
 
-// слушаем отправку формы
-//form.addEventListener('submit', function formSend(evt) {
-//   evt.preventDefault();
-//fetch('sendmail.php', {
-//    method: "POST",
-//    body: JSON.stringify({
-//        message: formData
-//    }),
-// });
-// if (res.ok) {
-//     return res.json();
-// } else {
-//    return Promise.reject(
-//        `Что-то пошло не так: Ошибка ${res.status} - ${res.statusText}`
-//     );
-// }
+// отправка формы 
 $(document).ready(function () {
 
     //E-mail Ajax Send
@@ -60,9 +28,4 @@ $(document).ready(function () {
         });
         return false;
     });
-
 });
-
-
-//});
-
